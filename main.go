bc5849f30e9ce1488a90bed311fa7e1a349f497b
Pedro Sousa <pedroteosousa@gmail.com>
2020-10-01 14:28:03 -0300

    Lint main.go with go fmt

--- main.go
+++ main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-    "time"
+	"context"
 	"fmt"
-    "context"
-    "os"
-    "os/exec"
+	"os"
+	"os/exec"
+	"time"
 )
 
 func main() {
 	actualOutput := make(chan []byte)
 	errorOutput := make(chan error)
 
-    ctx, _ := timerContext()
+	ctx, _ := timerContext()
 
-    file := []string{"./a"}
+	file := []string{"./a"}
 
-    inputFile, _ := os.Open("in")
+	inputFile, _ := os.Open("in")
 
-    go execute(ctx, file, inputFile, actualOutput, errorOutput)
+	go execute(ctx, file, inputFile, actualOutput, errorOutput)
 	select {
 	case <-ctx.Done(): // Tirando esse case os programas de RTE sao avaliados corretamente
-        fmt.Println("deu tle")
+		fmt.Println("deu tle")
 		return
-    case err := <-errorOutput:
-        fmt.Println("deu rte")
-        fmt.Println("%s", err)
+	case err := <-errorOutput:
+		fmt.Println("deu rte")
+		fmt.Println("%s", err)
 		return
 	case out := <-actualOutput:
-        fmt.Println("Compara as saidas")
-        stringOutput := string(out)
-        fmt.Println(stringOutput)
+		fmt.Println("Compara as saidas")
+		stringOutput := string(out)
+		fmt.Println(stringOutput)
 		return
 	}
 }
@@ -38,11 +38,11 @@ func main() {
 func execute(ctx context.Context, executable []string, inputFile *os.File, output chan<- []byte, errorOutput chan<- error) {
 	cmd := exec.CommandContext(ctx, executable[0], executable[1:]...)
 	cmd.Stdin = inputFile
-    fmt.Println("Pegando o output..")
+	fmt.Println("Pegando o output..")
 	programOutput, err := cmd.Output() // Nao ta conseguindo pegar o output de um arquivo que tenha dado RTE, tirar o case da linha 23 resolve
-    fmt.Println("pegou")
-    fmt.Println(err)
-    fmt.Println(programOutput)
+	fmt.Println("pegou")
+	fmt.Println(err)
+	fmt.Println(programOutput)
 	if err != nil {
 		errorOutput <- err
 		return
