b87caf4c8f31e2a4a98dd993d8a0ef192e8b84d6
Pedro Sousa <pedroteosousa@gmail.com>
2020-10-27 18:12:00 -0300

    Run multiple tests on Verdict
    Using filepath.Walk to list all files with .in as their extension and
    looking for the equivalent .out on the output folder.
    
    Running all tests sequentially and returning the first non-AC result
    
    Signed-off-by: "jiang.zhi@outlook.com"
    Signed-off-by: "alan.barzilay@gmail.com"
    Signed-off-by: "pedroteosousa@gmail.com"
    Signed-off-by: "gabfeol@gmail.com"

--- verdict/verdict.go
+++ verdict/verdict.go
@@ -1,13 +1,17 @@
 package verdict
 
 import (
+	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/maratona-run-time/Maratona-Runtime/comparator"
 	"github.com/maratona-run-time/Maratona-Runtime/executor"
 )
 
-func Verdict(timeout float32, executablePath string, inputFileName string, outputFileName string, result chan<- string) {
+func runTest(timeout float32, executablePath string, inputFileName string, outputFileName string, result chan<- string) {
 	statusChan := make(chan []string)
 
 	go executor.Execute(executablePath, inputFileName, timeout, statusChan)
@@ -29,4 +33,42 @@ func Verdict(timeout float32, executablePath string, inputFileName string, outpu
 			result <- "WA"
 		}
 	}
+}
+
+func Verdict(timeout float32, executablePath string, inputFilesFolder string, outputFilesFolder string, result chan<- string) {
+	var files [][2]string
+
+	root := inputFilesFolder
+	err := filepath.Walk(root, func(inputPath string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		basename := info.Name()
+		if filepath.Ext(basename) == ".in" {
+			testName := strings.TrimSuffix(basename, filepath.Ext(basename))
+			outputPath := filepath.Join(outputFilesFolder, testName+".out")
+			files = append(files, [2]string{inputPath, outputPath})
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, testFiles := range files {
+		testResult := make(chan string)
+		go runTest(timeout, executablePath, testFiles[0], testFiles[1], testResult)
+		switch <-testResult {
+		case "TLE":
+			result <- "TLE"
+			return
+		case "RTE":
+			result <- "RTE"
+			return
+		case "WA":
+			result <- "WA"
+			return
+		}
+	}
+	result <- "AC"
+	return
 }
