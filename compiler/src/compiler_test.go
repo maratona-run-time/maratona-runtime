950bf27221bc21aa1dae3ed2f7b16e28504f1174
gafeol <gabfeol@gmail.com>
2020-11-03 15:37:08 -0300

    Fix compiler test
    Fixes program names to new pattern, fixes deleting of remaining auxiliary files
    
    Signed-off-by: "pedroteosousa@gmail.com"
    Signed-off-by: "cainotis@usp.br"
    Signed-off-by: "jiang.zhi@outlook.com"
    Signed-off-by: "gabfeol@gmail.com"

--- compiler/src/compiler_test.go
+++ compiler/src/compiler_test.go
@@ -7,11 +7,11 @@ import (
 )
 
 func setup(lang string, file string) {
-	exec.Command("cp", "tests/"+file, "./program").Output()
+	exec.Command("cp", "tests/"+file, "./"+file).Output()
 }
 
 func teardown(file string) {
-	exec.Command("rm", "program.out", "program").Output()
+	exec.Command("rm", "program.out", file).Output()
 }
 
 func TestCompilation(t *testing.T) {
@@ -19,10 +19,10 @@ func TestCompilation(t *testing.T) {
 		lang string
 		file string
 	}{
-		{"C", "programa.c"},
-		{"Python", "programa.py"},
-		{"C++11", "programa.cpp"},
-		{"Go", "programa.go"},
+		{"C", "program.c"},
+		{"Python", "program.py"},
+		{"C++11", "program.cpp"},
+		{"Go", "program.go"},
 	}
 	for _, test := range tests {
 		t.Run(fmt.Sprintf("%s/%s", test.lang, test.file), func(t *testing.T) {
