b6f26fb8494bcd257885ea3a4c9db654de7265e5
gafeol <gabfeol@gmail.com>
2020-11-15 20:51:28 -0300

    Fix testing on travis and helper script

--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
+export CGO_ENABLED=0
 diff -u <(echo -n) <(gofmt -d ./)
-go test github.com/maratona-run-time/Maratona-Runtime/verdict/src -cover -v
-go test github.com/maratona-run-time/Maratona-Runtime/executor/src -cover -v
-go test github.com/maratona-run-time/Maratona-Runtime/compiler/src -cover -v
-go test github.com/maratona-run-time/Maratona-Runtime/comparator/src -cover -v
+go test ./... -cover -v
