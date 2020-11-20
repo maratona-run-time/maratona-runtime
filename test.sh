2e75f08850c870a897b21374cf9cdae4d40b3f59
Pedro Sousa <pedroteosousa@gmail.com>
2020-11-20 14:34:00 -0300

    Fix problem with test script
    Golang code for testing AC and CE were causing test.sh to fail.
    Going back to running `go test` for every package so those files are not
    considered.
    
    Also making travis run the test.sh script on root folder and changing
    the script to keep the exit status.
    
    Signed-off-by: "gabfeol@gmail.com"
    Signed-off-by: "pedroteosousa@gmail.com"

--- test.sh
+++ test.sh
@@ -1,3 +1,13 @@
+set -e
 export CGO_ENABLED=0
 diff -u <(echo -n) <(gofmt -d ./)
-go test ./... -cover -v | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
+
+test_folder() {
+    go test $1 -v -cover | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
+    go test $1 &> /dev/null
+}
+
+test_folder github.com/maratona-run-time/Maratona-Runtime/verdict/src
+test_folder github.com/maratona-run-time/Maratona-Runtime/compiler/src
+test_folder github.com/maratona-run-time/Maratona-Runtime/executor/src
+test_folder github.com/maratona-run-time/Maratona-Runtime/comparator/src
