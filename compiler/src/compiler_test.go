52280eee4e8b845cd33d770e082fc581346989b2
Cainotis <cainotis@gmail.com>
2020-12-01 16:26:27 -0300

    fixed compiler_test to use logger
    Signed-off-by: "cainotis@usp.br"
    Signed-off-by: "jiang.zhi@outlook.com"
    Signed-off-by: "alan.barzilay@gmail.com"

--- compiler/src/compiler_test.go
+++ compiler/src/compiler_test.go
@@ -4,7 +4,22 @@ import (
 	"fmt"
 	"os/exec"
 	"testing"
+	"github.com/rs/zerolog"
+	"os"
 )
+
+func initLogger() (zerolog.Logger){
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout}
+	logger := zerolog.
+					New(consoleWriter).
+					With().
+					Timestamp().
+					Str("MaRT", "executor").
+					Logger().
+					Level(zerolog.DebugLevel)
+	return logger
+}
 
 func setup(lang string, file string) {
 	exec.Command("cp", "tests/"+file, "./"+file).Output()
@@ -15,6 +30,7 @@ func teardown(file string) {
 }
 
 func TestCompilation(t *testing.T) {
+	logger := initLogger()
 	tests := []struct {
 		lang string
 		file string
@@ -27,7 +43,7 @@ func TestCompilation(t *testing.T) {
 	for _, test := range tests {
 		t.Run(fmt.Sprintf("%s/%s", test.lang, test.file), func(t *testing.T) {
 			setup(test.lang, test.file)
-			_, err := Compile(test.lang, test.file)
+			_, err := Compile(test.lang, test.file, logger)
 			if err != nil {
 				t.Errorf("Compilation failed!")
 			}
