8f79b6fd403a13d434c8651d2e0df4ceb89e9a0b
Pedro Sousa <pedroteosousa@gmail.com>
2020-12-08 17:03:14 -0300

    Add tests for compiler server
    Remove source files on compiler serve so tests can run multiple times.
    
    Signed-off-by: "alan.barzilay@gmail.com"
    Signed-off-by: "cainotis@usp.br"
    Signed-off-by: "gabfeol@gmail.com"
    Signed-off-by: "jiang.zhi@outlook.com"
    Signed-off-by: "pedroteosousa@gmail.com"

--- compiler/main.go
+++ compiler/main.go
@@ -29,24 +29,7 @@ var sourceFileName = map[string]string{
 	"Go":     "program.go",
 }
 
-func main() {
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout}
-	logFile, logErr := os.OpenFile("compiler.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
-	defer logFile.Close()
-	if logErr != nil {
-		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-		log.Fatal().Err(logErr).Msg("Could not create log file")
-	}
-	multi := zerolog.MultiLevelWriter(consoleWriter, logFile)
-	logger := zerolog.
-		New(multi).
-		With().
-		Timestamp().
-		Str("MaRT", "compiler").
-		Logger().
-		Level(zerolog.DebugLevel)
-
+func createCompilerServer(logger zerolog.Logger) *martini.ClassicMartini {
 	m := martini.Classic()
 	m.Post("/", binding.MultipartForm(FileForm{}), func(rs http.ResponseWriter, rq *http.Request, req FileForm) {
 		fileName := sourceFileName[req.Language]
@@ -72,6 +55,13 @@ func main() {
 		f.Close()
 		program.Close()
 		ret, compilerErr := compiler.Compile(req.Language, fileName, logger)
+		err := os.Remove(fileName)
+		if err != nil {
+			msg := "Could not remove source file"
+			logger.Error().
+				Err(err).
+				Msg(msg)
+		}
 		if compilerErr != nil {
 			msg := "An error occurred while trying compile program in language '" + req.Language + "'"
 			logger.Error().
@@ -82,5 +72,27 @@ func main() {
 		}
 		http.ServeFile(rs, rq, ret)
 	})
+	return m
+}
+
+func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout}
+	logFile, logErr := os.OpenFile("compiler.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
+	defer logFile.Close()
+	if logErr != nil {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+		log.Fatal().Err(logErr).Msg("Could not create log file")
+	}
+	multi := zerolog.MultiLevelWriter(consoleWriter, logFile)
+	logger := zerolog.
+		New(multi).
+		With().
+		Timestamp().
+		Str("MaRT", "compiler").
+		Logger().
+		Level(zerolog.DebugLevel)
+
+	m := createCompilerServer(logger)
 	m.RunOnAddr(":8080")
 }
