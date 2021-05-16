2ee9a35810391c88dc97d06684d3f130fd4d2e1e
Gabriel Fernandes de Oliveira <gabfeol@gmail.com>
2021-05-16 18:42:47 -0300

    Add docker testing image
    Running ./testDocker.sh executes tests on an alpine environment.
    Also simplify test.sh, removed parenthesis so that it can run on alpine.

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 export CGO_ENABLED=0
-diff -u <(echo -n) <(gofmt -d ./)
+gofmt -d ./
 go test ./... -v -cover | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
